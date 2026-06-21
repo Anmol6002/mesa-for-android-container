9024010119433b0592112cab8bb87e0a89c9e139
Anmol Singh <162159622+Anmol6002@users.noreply.github.com>
2026-06-21 17:24:20 +0530

    Update freedreno_devices.py

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -452,7 +452,7 @@ add_gpus([
         GPUId(610),
         GPUId(612), # TODO: Test it, based only on libwrapfake dumps
         GPUId(613),
-], A6xxGPUInfo(
+    ], A6xxGPUInfo(
         CHIP.A6XX,
         [a6xx_base, a6xx_gen1_low],
         num_ccu = 1,
